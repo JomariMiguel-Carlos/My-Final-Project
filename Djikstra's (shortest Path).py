726f995c6e51c4dceb626c649afdc8cdbf45a286
JomariMiguel-Carlos <jomarimadla0@gmail.com>
2023-02-11 18:09:01 +0800

    put the start and endd point

--- Djikstra's (shortest Path).py
+++ Djikstra's (shortest Path).py
@@ -19,3 +19,15 @@ u = -1
 s = 0
 e = 0
 array_path = []
+
+def dijk():
+    global u, s, e
+    s = start.grid_info()["row"] * 30 + start.grid_info()["column"]
+    e = end.grid_info()["row"] * 30 + end.grid_info()["column"]
+    d[s] = 0
+    for i in range(570):
+        mini = 999
+        for j in range(570):
+            if (d[j] < mini) and (visited[j] == 0):
+                mini = d[j]
+                u = j
