6d0cd068f8e8b793f62217d95af7f7c4dd5d7f12
JomariMiguel-Carlos <jomarimadla0@gmail.com>
2023-02-11 18:03:20 +0800

    setting all the needed/important points

--- Djikstra's (shortest Path).py
+++ Djikstra's (shortest Path).py
@@ -3,3 +3,19 @@ from tkinter import *
 root = Tk()
 root.geometry("662x500+300+100")
 root.title("Shortest path - Dijkstra's algorithm")
+
+grid = []
+matrix = []
+c = 0
+count = 0
+start, end = None, None
+btn = None
+rst = None
+find = False
+d = []
+p = []
+visited = []
+u = -1
+s = 0
+e = 0
+array_path = []
